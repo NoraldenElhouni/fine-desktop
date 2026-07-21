352e2279a6fed8063021c4f50d30b70e5ca450ae
NoraldenElhouni <noralden.honi@gmail.com>
2026-07-21 11:36:46 +0200

    install dependencies

--- src/app.tsx
+++ src/app.tsx
@@ -2,4 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+root.render(
+  <h2 className="text-2xl font-bold text-gray-100">Hello from React!</h2>,
+);
